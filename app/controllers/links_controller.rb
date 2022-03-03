306b386a0b5c8cdf017482052a2ddedab2f4553b
Fly Chang <leo424y@gmail.com>
2022-03-03 12:49:12 +0800

    🐛 links by tag

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -119,7 +119,7 @@ class LinksController < ApplicationController
   end
 
   def tag
-    @links = Link.find(Like.where(mail: params[:tag]).ids).reverse
+    @links = Link.where(id: Like.where(mail: params[:tag]).pluck(:link_id)).reverse
   end
 
   def skip
